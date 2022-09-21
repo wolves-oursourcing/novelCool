4123cc7e511212148d6b2d26616a6be3b26a04ff
Vinamilk's <toinv2610@gmail.com>
2022-09-21 20:54:10 +0700

    update ui detial page

--- wrapper/novel/comments.tsx
+++ wrapper/novel/comments.tsx
@@ -1,7 +1,41 @@
-interface IPropsComment {}
+import { PlusOutlined, StarFilled, StarOutlined } from '@ant-design/icons';
+import { Divider, Input, Typography, Upload } from 'antd';
+import { useState } from 'react';
+import { Comment } from '../../api/comment';
 
+interface IPropsComment {
+  comments?: Comment[];
+}
+
+const { Title, Text, Paragraph } = Typography;
+const { TextArea } = Input;
 const CommentView = (props: IPropsComment) => {
-  return <div>comment</div>;
+  const { comments } = props;
+  const [comment, setComment] = useState<Comment>();
+  return (
+    <div className="comment-view">
+      <Title level={4}>{comments && comments.length} Comments</Title>
+      <Divider className="detail-page-divider" />
+      <div className="write-comment">
+        <div className="rate">
+          <Text>Your rating fo this book is: </Text>
+          <div className="list-star">
+            <StarOutlined />
+            <StarFilled className="star-active" />
+          </div>
+        </div>
+        <div className="message">
+          <TextArea placeholder="Write down your comments here"></TextArea>
+          <Upload action="/upload.do" listType="picture-card">
+            <div>
+              <PlusOutlined />
+              <div style={{ marginTop: 8 }}>Upload</div>
+            </div>
+          </Upload>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentView;
