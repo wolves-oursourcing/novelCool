05c623707ece08790fadba119a5bc76e2106d589
Vinamilk's <toinv2610@gmail.com>
2022-11-24 22:47:47 +0700

    add style for novel description

--- api/configs.ts
+++ api/configs.ts
@@ -1,13 +1,12 @@
 export const Config = {
-  // URL_API: 'https://api.anime.wildwolves.shop/',
-  URL_API: 'http://157.245.152.118:3001/',
+  URL_API: 'https://api.anime.wildwolves.shop/',
+  // URL_API: 'http://157.245.152.118:3001/',
   // URL_API: 'http://localhost:3002/',
   TOKEN: 'token',
   USER: 'user',
-  FACEBOOK_APP_ID: "658430965696356",
-  GOOGLE_CLIENT_ID:
-      "498077768323-gb1slr9b4k64o8vse1ds3p0q4979439s.apps.googleusercontent.com",
-  CLIENT_SECRET: "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",
+  FACEBOOK_APP_ID: '658430965696356',
+  GOOGLE_CLIENT_ID: '498077768323-gb1slr9b4k64o8vse1ds3p0q4979439s.apps.googleusercontent.com',
+  CLIENT_SECRET: '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'
 };
 
 export const RESPONSE_STATUS = {
