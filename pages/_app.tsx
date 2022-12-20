4aea631f7c8025f11c92be9fa7667d2d0271a5c9
nghi12 <nghilk69@gmail.com>
2022-12-20 12:31:54 +0700

    update gg ananlytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,28 @@ import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import { LayoutProvider } from '../layout/DefaultLayout';
 import '../styles/global.scss';
+import {useEffect, useState} from "react";
+import googleServices from "../services/google.services";
 
 function App({ Component, pageProps }: AppProps) {
-  return <LayoutProvider>{<Component {...pageProps} className="container" />}</LayoutProvider>;
+  const [analyticsId, setAnalyticsId] = useState('');
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const host = window.location.hostname;
+    const res = await googleServices.getAllGoogleData({domain: host});
+    if(res[0]) {
+      setAnalyticsId(res[0].codeAnalytics);
+    }
+    console.log(res, 'data');
+  }
+
+  return (<>
+    <script async src={`https://www.googletagmanager.com/gtag/js?id=${analyticsId}`}/>
+    <LayoutProvider>{<Component {...pageProps} className="container" />}</LayoutProvider>);
+  </>)
 }
 
 export default App;
